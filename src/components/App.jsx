a848550117cdb130b871000059ba96015fec21c0
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-07-01 13:26:15 +0200

    Completed hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,12 +9,7 @@ import { Filter } from './Filter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -54,6 +49,30 @@ export class App extends Component {
       filter: evt.target.value,
     });
   };
+
+  componentDidMount() {
+    try {
+      const localContacts = localStorage.getItem('contacts') ?? '[]';
+
+      this.setState({
+        contacts: JSON.parse(localContacts),
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      try {
+        const localContacts = JSON.stringify(this.state.contacts);
+
+        localStorage.setItem('contacts', localContacts);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
 
   render() {
     return (
