39a725e75c335a28240e284b720a61fbd453f744
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-07-04 21:41:46 +0200

    Updated App to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,22 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { Section } from './Section';
-import theme from 'theme';
 import { nanoid } from 'nanoid';
+import { Section } from './Section';
 import { ContactForm } from './ContactForm';
 import { ContactList } from './ContactList';
 import { Filter } from './Filter';
+import { useLocalStorage } from 'hooks/useLocalStorage';
+import theme from 'theme';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useLocalStorage('contacts', []);
+  const [filter, setFilter] = useState('');
 
-  createContact = ({ name, number }) => {
+  const createContact = ({ name, number }) => {
     name = name.trim();
 
     if (
-      this.state.contacts.find(
+      contacts.find(
         contact => name.toLowerCase() === contact.name.toLowerCase()
       )
     ) {
@@ -25,69 +24,30 @@ export class App extends Component {
       return;
     }
 
-    this.setState(prev => {
-      return {
-        contacts: [...prev.contacts, { name, number, id: nanoid() }],
-      };
-    });
+    setContacts(prev => [...prev, { name, number, id: nanoid() }]);
   };
 
-  deleteContact = id => {
-    this.setState(prev => ({
-      contacts: prev.contacts.filter(contact => contact.id !== id),
-    }));
+  const deleteContact = id => {
+    setContacts(prev => prev.filter(contact => contact.id !== id));
   };
 
-  filterContacts = () => {
-    return this.state.contacts.filter(({ name }) =>
-      name.toLowerCase().includes(this.state.filter)
-    );
+  const filterContacts = () => {
+    return contacts.filter(({ name }) => name.toLowerCase().includes(filter));
   };
 
-  handleFilter = evt => {
-    this.setState({
-      filter: evt.target.value.toLowerCase(),
-    });
+  const handleFilter = evt => {
+    setFilter(evt.target.value.toLowerCase());
   };
 
-  componentDidMount() {
-    try {
-      const localContacts = localStorage.getItem('contacts') ?? '[]';
-
-      this.setState({
-        contacts: JSON.parse(localContacts),
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      try {
-        const localContacts = JSON.stringify(this.state.contacts);
-
-        localStorage.setItem('contacts', localContacts);
-      } catch (err) {
-        console.error(err);
-      }
-    }
-  }
-
-  render() {
-    return (
-      <ThemeProvider theme={theme}>
-        <Section title="Phonebook">
-          <ContactForm onSubmit={this.createContact} />
-        </Section>
-        <Section title="Contacts">
-          <Filter onChange={this.handleFilter} />
-          <ContactList
-            contacts={this.filterContacts()}
-            onDelete={this.deleteContact}
-          />
-        </Section>
-      </ThemeProvider>
-    );
-  }
-}
+  return (
+    <ThemeProvider theme={theme}>
+      <Section title="Phonebook">
+        <ContactForm onSubmit={createContact} />
+      </Section>
+      <Section title="Contacts">
+        <Filter onChange={handleFilter} />
+        <ContactList contacts={filterContacts()} onDelete={deleteContact} />
+      </Section>
+    </ThemeProvider>
+  );
+};
